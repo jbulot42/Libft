1bec65e1345c62ab516519657cffa99c1c743256
Julien BULOT <jbulot@student.42.fr>
2026-02-06 11:48:28 +0100

    ft_strlcpy & ft_strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbulot <jbulot@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/06 11:41:29 by jbulot            #+#    #+#             */
+/*   Updated: 2026/02/06 12:10:28 by jbulot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t siz)
+{
+	size_t	i;
+	size_t	dlen;
+
+	dlen = ft_strlen(dst);
+	if (siz <= dlen)
+		return (siz + ft_strlen(src));
+	i = 0;
+	while (src[i] && dlen + i < siz - 1)
+	{
+		dst[dlen + i] = src[i];
+		i++;
+	}
+	dst[dlen + i] = '\0';
+	return (dlen + ft_strlen(src));
+}
---
+++ ft_strlcpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbulot <jbulot@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/06 11:38:08 by jbulot            #+#    #+#             */
+/*   Updated: 2026/02/06 12:10:33 by jbulot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t siz)
+{
+	size_t	i;
+
+	if (!siz)
+		return (ft_strlen(src));
+	i = 0;
+	while (src[i] && i < siz - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (ft_strlen(src));
+}
