55701ed35946ef533a79d16ed355e41a32eb2e6d
Julien BULOT <jbulot@student.42.fr>
2026-02-06 16:31:40 +0100

    ft_split

---
+++ ft_split.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbulot <jbulot@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/06 16:13:11 by jbulot            #+#    #+#             */
+/*   Updated: 2026/02/06 16:30:28 by jbulot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t	count_words(char const *s, char c)
+{
+	size_t	count;
+	size_t	i;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		if (s[i])
+		{
+			count++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+	}
+	return (count);
+}
+
+static void	free_all(char **tab, size_t i)
+{
+	while (i > 0)
+		free(tab[--i]);
+	free(tab);
+}
+
+static char	**fill_tab(char const *s, char c, char **tab)
+{
+	size_t	i;
+	size_t	j;
+	size_t	start;
+
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		start = i;
+		while (s[i] && s[i] != c)
+			i++;
+		if (i > start)
+		{
+			tab[j] = ft_substr(s, start, i - start);
+			if (!tab[j])
+				return (free_all(tab, j), NULL);
+			j++;
+		}
+	}
+	tab[j] = NULL;
+	return (tab);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**tab;
+
+	if (!s)
+		return (NULL);
+	tab = malloc(sizeof(*tab) * (count_words(s, c) + 1));
+	if (!tab)
+		return (NULL);
+	return (fill_tab(s, c, tab));
+}
