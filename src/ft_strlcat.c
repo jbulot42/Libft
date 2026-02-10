c3c832a0574336364f83ddac969fdc56ec859b4d
Julien BULOT <jbulot@student.42.fr>
2026-02-10 11:32:38 +0100

    ft_strlcat

---
+++ src/ft_strlcat.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbulot <jbulot@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/10 11:22:36 by jbulot            #+#    #+#             */
+/*   Updated: 2026/02/10 11:30:49 by jbulot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t siz)
+{
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	i;
+
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (siz <= dst_len)
+		return (siz + src_len);
+	i = 0;
+	while (i < siz - dst_len - 1 && src[i] != '\0')
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (dst_len + src_len);
+}
