5eea0f9e3c2b1666f349f52b4845c747b4761e82
Julien BULOT <jbulot@student.42.fr>
2026-02-10 14:28:55 +0100

    ft_memcmp

---
+++ src/ft_memcmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbulot <jbulot@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/10 14:25:47 by jbulot            #+#    #+#             */
+/*   Updated: 2026/02/10 14:28:00 by jbulot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*a;
+	unsigned char	*b;
+
+	a = (unsigned char *)s1;
+	b = (unsigned char *)s2;
+	while (n--)
+	{
+		if (*a != *b)
+			return (*a - *b);
+		a++;
+		b++;
+	}
+	return (0);
+}
